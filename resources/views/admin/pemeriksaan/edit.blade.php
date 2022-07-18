e582bf9ccf34edda226f93165cebbb5b8168b5f1
RonnyFebrianSaputra <82427839+ronnyfebriansaputra12@users.noreply.github.com>
2022-07-18 17:44:16 +0700

    Update edit.blade.php
    halaman edit pemeriksaan

---
+++ resources/views/admin/pemeriksaan/edit.blade.php
@@ -0,0 +1,105 @@
+@extends('admin.layouts.main')
+
+@section('container')
+
+<div class="card">
+    <h5 class="card-header">Pemeriksaan</h5>
+    <div class="card-body">
+        <form action ="/pemeriksaan/{{ $pemeriksaans->id }}" method="post">
+            @method('PUT')
+            @csrf
+            <div class="row">
+                <div class="col-md-6">
+            <div class="mb-3">
+                <label for="jurusan" class="form-label">Antrian</label>
+                <select class="form-select" name="antrian_id" aria-label="Default select example">
+                    <option selected></option>
+                    @foreach($antrians as $antrian)
+                        @if (old('antrian_id',$pemeriksaans->antrian_id) == $antrian->id)
+                            <option value="{{ $antrian->id }}" selected>{{ $antrian->no_antrian }}</option>
+                        @else
+                            <option value="{{ $antrian->id }}">{{ $antrian->no_antrian }}</option>  
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <div class="col-md-6">
+            <div class="mb-3">
+                <label for="jurusan" class="form-label">Pasien</label>
+                <select class="form-select" name="pasien_id" aria-label="Default select example">
+                    <option selected></option>
+                    @foreach($pasiens as $pasien)
+                        @if (old('pasien_id',$pemeriksaans->pasien_id) == $pasien->id)
+                            <option value="{{ $pasien->id }}" selected>{{ $pasien->nama_pasien }} - {{ $pasien->nik }}</option>
+                        @else
+                            <option value="{{ $pasien->id }}">{{ $pasien->nama_pasien }} - {{ $pasien->nik }}</option>  
+                        @endif
+                    @endforeach
+                </select>
+            </div>
+        </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Suhu Badan</label>
+                    <input type="text" class="form-control @error('suhu_badan') is-invalid @enderror" id="suhu_badan" name="suhu_badan" value="{{old('suhu_badan',$pemeriksaans->suhu_badan)}}"  autofocus>
+                    @error('suhu_badan')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Tekanan Darah</label>
+                    <input type="text" class="form-control @error('tekanan_darah') is-invalid @enderror" id="tekanan_darah" name="tekanan_darah" value="{{old('tekanan_darah',$pemeriksaans->tekanan_darah)}}"  autofocus>
+                    @error('tekanan_darah')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>   
+            </div>
+        </div>
+
+        <div class="mb-3">
+            <label for="exampleFormControlInput1" class="form-label">Keluhan</label>
+            <input type="text" class="form-control @error('keluhan') is-invalid @enderror" id="keluhan" name="keluhan" value="{{old('keluhan',$pemeriksaans->keluhan)}}"  autofocus>
+            @error('keluhan')
+                <div class="invalid-feedback">
+                    {{ $message }}
+                </div>
+            @enderror
+        </div>
+            
+        <div class="mb-3">
+            <label for="status_pemeriksaan" class="form-label">Status Pemeriksaan</label>
+            <select class="form-select" name="status_pemeriksaan" aria-label="Default select example">
+                <option selected>Status</option>
+                <option value="belum" 
+                @if ($pemeriksaans['status_pemeriksaan']=="belum")
+                    {{ 'selected' }}
+                @endif>Belum</option>
+                <option value="selesai" @if ($pemeriksaans['status_pemeriksaan']=="selesai"){{ 'selected' }}@endif>Selesai</option>
+                    
+            </select>
+        </div>
+
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="submit" name="submit" class="btn btn-primary">Tambah Data</button>
+        </div>
+
+        </form>
+    </div>
+</div>
+
+
+@endsection
