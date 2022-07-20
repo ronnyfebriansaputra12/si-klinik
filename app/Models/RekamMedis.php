64d2cea3f7ac3f614a385ec791ddf518419de900
RonnyFebrianSaputra <82427839+ronnyfebriansaputra12@users.noreply.github.com>
2022-07-20 08:23:36 +0700

    Update RekamMedis.php
    Join Rekam Medis dengan dokter dan pemeriksaan

--- app/Models/RekamMedis.php
+++ app/Models/RekamMedis.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class RekamMedis extends Model
 {
     use HasFactory;
+    protected $guarded=[];
+
+    public function pemeriksaan()
+    {
+        return $this->belongsTo(Pemeriksaan::class);
+    }
+
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class);
+    }
 }
